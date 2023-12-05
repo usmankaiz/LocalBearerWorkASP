f5ad27c22479da6de3e4cd04cca6ffd03ff62138
Muhammad Usman <usman@wondertree.co>
2023-12-06 00:16:55 +0500

    First Commit

---
+++ BearerTokenLocalStore/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BearerTokenLocalStore
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BearerTokenLocalStore/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BearerTokenLocalStore.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
